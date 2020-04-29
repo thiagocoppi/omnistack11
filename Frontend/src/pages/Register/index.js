d52739e09dd8514f9f794db3f1668de1b0e1dc80
Thiago Coppi <coppithiago@gmail.com>
2020-04-29 10:16:44 -0300

    Correção no label e rota

--- Frontend/src/pages/Register/index.js
+++ Frontend/src/pages/Register/index.js
@@ -43,9 +43,9 @@ export default function Register() {
             Faça seu cadastro, entre na plataforma e ajuda pessoas a encontrarem
             os casos da sua ONG.
           </p>
-          <Link className="back-link" to="/register">
+          <Link className="back-link" to="/">
             <FiArrowLeft size={16} color="#E02041" />
-            Não tenho cadastro
+            Voltar
           </Link>
         </section>
         <form onSubmit={handleRegister}>
